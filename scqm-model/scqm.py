04cd753d436664dd888919cff6dcf00015d25f08
Wojciech Frącz <fraczwojciech@gmail.com>
2018-09-11 11:54:32 +0200

    Both models with GUI are working

--- scqm-model/scqm.py
+++ scqm-model/scqm.py
@@ -25,7 +25,7 @@ with ascqmGraph.as_default():
     ascqm_train_outputs = ascqmGraph.get_tensor_by_name("train_outputs:0")
     ascqm_seqlen = ascqmGraph.get_tensor_by_name("seqlen:0")
     ascqm_prediction = ascqmGraph.get_tensor_by_name("prediction:0")
-    inputSize = ascqm_train_inputs.get_shape().as_list()[1]
+    ascqmInputSize = ascqm_train_inputs.get_shape().as_list()[1]
     ascqm_saver.restore(ascqmSession, tf.train.latest_checkpoint(ascqmPath))
 
     @app.route("/ascqm", methods=['POST', 'GET'])
@@ -33,79 +33,58 @@ with ascqmGraph.as_default():
         r = requests.post("http://parser:8080/parse", data={'source': request.get_json()['source']})
         data = r.json()
         for i, methodDef in enumerate(data):
-            if len(data[i]['tokens']) <= inputSize:
+            if len(data[i]['tokens']) <= ascqmInputSize:
                 arr = np.array([data[i]['tokens']])
-                input = np.pad(arr, ((0,0), (0, inputSize-len(data[i]['tokens']))), 'constant')
+                input = np.pad(arr, ((0,0), (0, ascqmInputSize-len(data[i]['tokens']))), 'constant')
                 seqlen = np.array([len(data[i]['tokens'])])
                 pred = ascqmSession.run(ascqm_prediction, feed_dict={ascqm_train_inputs: input, ascqm_seqlen: seqlen})
                 data[i]['prediction'] = pred.tolist()[0]
         json_data = json.dumps(data)
         return json_data
 
-rsavePath = '../trained/code-fracz-645/rscqm'
-
-#batch_before, batch_after, seqlen_before, seqlen_after, batch_labels = dataset.next(5)
-#print("BEFORE:\n", batch_before, seqlen_before, "\nAFTER:\n", batch_after, seqlen_after, "\nLABELS:\n", batch_labels)
-
-
-############################################ RNN
-
-"""
+rscqmPath = '../trained/code-fracz-645/rscqm'
 rscqmGraph = tf.Graph()
 rscqmSession = tf.Session(graph=rscqmGraph)
-with rscqmGraph.as_default():
-    print("model meta")
-    rscqmSaver = tf.train.import_meta_graph(rsavePath + '/model.meta')
 
+with rscqmGraph.as_default():
+    rscqmSaver = tf.train.import_meta_graph(rscqmPath + '/model.meta')
     train_inputs_before = rscqmGraph.get_tensor_by_name("train_inputs_before:0")
     train_inputs_after = rscqmGraph.get_tensor_by_name("train_inputs_after:0")
     train_outputs = rscqmGraph.get_tensor_by_name("train_outputs:0")
     seqlen_before = rscqmGraph.get_tensor_by_name("seqlen_before:0")
     seqlen_after = rscqmGraph.get_tensor_by_name("seqlen_after:0")
     prediction = rscqmGraph.get_tensor_by_name("prediction:0")
-
-#    print(str([n.name for n in tf.get_default_graph().as_graph_def().node]))
-
-    print("model restore")
-    rscqmSaver.restore(rscqmSession, tf.train.latest_checkpoint(rsavePath))
-    print("savePath")
-#    all_vars = tf.get_collection('vars')
-#    for v in all_vars:
-#        v_ = sess.run(v)
-#        print(v_)
-    print("testin")
-    test_before, test_after, test_seqlen_before, test_seqlen_after = rdataset.test()
+    rscqmSaver.restore(rscqmSession, tf.train.latest_checkpoint(rscqmPath))
+    rscqmInputSize = train_inputs_before.get_shape().as_list()[1]
 
     @app.route("/rscqm", methods=['POST', 'GET'])
     def predictRscqm():
-        start = time.time()
+        jsonBefore = requests.post("http://parser:8080/parse", data={'source': request.get_json()['sourceBefore']}).json()
+        jsonAfter = requests.post("http://parser:8080/parse", data={'source': request.get_json()['sourceAfter']}).json()
+        result = {
+            'astBefore': jsonBefore,
+            'astAfter': jsonAfter,
+            'predictions': []
+        }
 
-        #data = request.data.decode("utf-8")
-        #if data == "":
-        #    params = request.form
-        #    x_in = json.loads(params['x'])
-        #else:
-        #    params = json.loads(data)
-        #    x_in = params['x']
-
-        ##################################################
-        # Tensorflow part
-        ##################################################
-        pred = rscqmSession.run(prediction, feed_dict={train_inputs_before: test_before,
-                                                             train_inputs_after: test_after,
-                                                             seqlen_before: test_seqlen_before,
-                                                             seqlen_after: test_seqlen_after})
-        ##################################################
-        # END Tensorflow part
-        ##################################################
-
-        json_data = json.dumps({'pred': pred.tolist()})
-        print("Time spent handling the request: %f" % (time.time() - start))
-
+        for i, methodDef in enumerate(jsonBefore):
+            methodDefsBefore = [m for m in jsonBefore if m['methodName'] == methodDef['methodName']]
+            methodDefsAfter = [m for m in jsonAfter if m['methodName'] == methodDef['methodName']]
+            if len(methodDefsBefore) == len(methodDefsAfter) and len(methodDefsBefore) == 1 and methodDefsBefore[0]['tokens'] != methodDefsAfter[0]['tokens'] and len(methodDefsBefore[0]['tokens']) <= rscqmInputSize and len(methodDefsAfter[0]['tokens']) <= rscqmInputSize:
+                arrBefore = np.array([methodDefsBefore[0]['tokens']])
+                arrAfter = np.array([methodDefsAfter[0]['tokens']])
+                inputBefore = np.pad(arrBefore, ((0,0), (0, rscqmInputSize-len(methodDefsBefore[0]['tokens']))), 'constant')
+                inputAfter = np.pad(arrAfter, ((0,0), (0, rscqmInputSize-len(methodDefsAfter[0]['tokens']))), 'constant')
+                seqlenBefore = np.array([len(methodDefsBefore[0]['tokens'])])
+                seqlenAfter = np.array([len(methodDefsAfter[0]['tokens'])])
+                pred = rscqmSession.run(prediction, feed_dict={train_inputs_before: inputBefore,
+                                                                     train_inputs_after: inputAfter,
+                                                                     seqlen_before: seqlenBefore,
+                                                                     seqlen_after: seqlenAfter})
+                result['predictions'].append({'methodName': methodDef['methodName'], 'methodBefore': methodDefsBefore[0], 'methodAfter': methodDefsAfter[0], 'prediction': pred.tolist()[0]});
+        json_data = json.dumps(result)
         return json_data
 
-    predictRscqm()
-"""
 @app.route("/parse", methods=['POST'])
 def parser():
     r = requests.post("http://parser:8080/parse", data={'source': request.form.get('source')})
